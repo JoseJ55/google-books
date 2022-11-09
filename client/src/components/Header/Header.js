291d70ea2e53e7d106f54dc8789c799c52f6f73a
Jose Jimenez <josejimenez826@gmail.com>
2022-11-08 22:20:13 -0800

    updated the header of the website

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return(
         <div id="header">
             <div id="headerArea">
-                <h1>Google Books Search</h1>
+                <h1><view id="google-text"><text id="google-blue">G</text><text id="google-red">o</text><text id="google-yellow">o</text><text id="google-blue">g</text><text id="google-green">l</text><text id="google-red">e</text></view> Books Search</h1>
                 <h2>Search for and Save Books of Interest</h2>
             </div>
         </div>
